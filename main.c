3aac2064021ad7c19509598162cc7f8bd2465ffa
curtis <curteins@vt.edu>
2014-09-14 23:46:21 -0400

    Added some UART testing code

--- main.c
+++ main.c
@@ -330,9 +330,55 @@ void main(void) {
     // It is also slow and is blocking, so it will perturb your code's operation
     // Here is how it looks: printf("Hello\r\n");
 
-    TRISBbits.TRISB1 = 0;
+    //CURTIS CHANGES
+    // Set the tri state logic to output
     TRISBbits.TRISB0 = 0;
-    
+    TRISBbits.TRISB1 = 0;
+
+    // Sending signals high and low to get output on Logic Analyzer
+    LATBbits.LATB0 = 0x1;
+    LATBbits.LATB0 = 0x0;
+    LATBbits.LATB0 = 0x1;
+    LATBbits.LATB0 = 0x0;
+    LATBbits.LATB0 = 0x1;
+    LATBbits.LATB0 = 0x0;
+
+    // Configuring pins for EUSART1
+    RCSTA1bits.SPEN = 0x1; // Set SPEN bit to 1
+    TRISCbits.TRISC7 = 0x1; // Set to 1 for EUSART1
+    TRISCbits.TRISC6 = 0x0; // Set to 0 for asynchronous master mode
+
+    // Register 20-1: TXSTAx: TRANSMIT STATUS AND CONTROL REGISTER (ACCESS FADh, FA8h)
+    TXSTA1bits.TXEN = 0x1; // Transmit is enabled
+    TXSTA1bits.SYNC = 0x0; // Set EUSART mode to Asynchronous
+    TXSTA1bits.BRGH = 0x1; // High speed Baud Rate
+
+    // Register 20-2: RCSTAx: RECEIVE STATUS AND CONTROL REGISTER (ACCESS FACh, F9Ch)
+    RCSTA1bits.SPEN = 0x1; // Serial port is enabled
+
+    // Register 20-3: BAUDCONx: BAUD RATE CONTROL REGISTER (ACCESS F7Eh, F7Ch)
+    BAUDCON1bits.BRG16 = 0x1; // 16-bit Baud Rate Generator
+
+    // Set up baud rate generator for 57,600. See Baud Rate formula on page 328 of datasheet.
+    // Actual baud rate generated here is 57,692 which is 0.16% error.
+    SPBRGH1 = 0x0; // High byte
+    SPBRG1 = 0xCF; // Low byte, 207 in decimal
+
+    // Load Data into the TXREG1 register, which will start the transmission
+    TXREG1 = 0x55;
+
+    // Infinite for loop to test UART
+    //for(;;){}
+    //Set the I2C_ISR to high when we are in the interrupt handler
+    /*
+    DEBUG_ON(I2C_ISR);
+    DEBUG_OFF(I2C_ISR);
+    DEBUG_ON(I2C_ISR);
+    DEBUG_OFF(I2C_ISR);
+    DEBUG_ON(I2C_ISR);
+    DEBUG_OFF(I2C_ISR);
+    */
+    // END CURTIS CHANGES
 
     // loop forever
     // This loop is responsible for "handing off" messages to the subroutines
